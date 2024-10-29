32047583417a556aa17d01d29057d021a3c6c571
Mami Ishii <nozaru.yeah@gmail.com>
2024-10-29 15:01:34 +0900

    changed the button name

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function App() {
         onChange={(e) => setRoomName(e.target.value)}
         placeholder="Room name"
       />
-      <button onClick={addRoom}>Create Room</button>
+      <button onClick={addRoom}>Add Room</button>
       <ul>
         {rooms.map((room) => (
           <li key={room.id}>
